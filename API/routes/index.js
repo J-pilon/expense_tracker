2b873d4a83320017159ca3c823dd9c9198bf99bd
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 16:38:30 -0700

    mysql connection option

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const connection = require('../db/mysql_setup')
+const conn = require('../db/mysql_setup')
 
 /* GET home page. */
 router.get('/api/expenses', function(req, res, next) {
-  connection.query("SELECT * FROM expenses", (err, rows) => {
+  conn.query("SELECT * FROM expenses", (err, rows) => {
     if (err) throw err;
     // console.log('the data from the expenses table \n', rows);
     res.json(rows);
-    connection.end();
+    conn.end();
   })
 });
 
