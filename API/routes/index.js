a27ef96619d3028135715b06bba1ff266efc89db
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 16:14:19 -0700

    api/expenses route queries all the expenses from the database

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../db/mysql_setup')
 
 /* GET home page. */
 router.get('/api/expenses', function(req, res, next) {
-  res.send('respond with a resource');
+  connection.query("SELECT * FROM expenses", (err, rows) => {
+    if (err) throw err;
+    // console.log('the data from the expenses table \n', rows);
+    res.json(rows);
+    connection.end();
+  })
 });
+
+
 
 module.exports = router;
