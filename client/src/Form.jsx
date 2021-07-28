cb20e4a2f84d1ccada9e7b678da89743205ba567
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 21:25:04 -0700

    removed for attribute from label

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,12 +1,19 @@
 export default function Form() {
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    console.log("submitted!")
+  }
+
   return (
     <div>
-      <form action="/api" method="post">
-        <label for="exp-name">Expense name:</label>
+      <form onSubmit={e => submitHandler(e)}>
+        <label>Expense name:</label>
         <input type="text" id="exp-name"></input>
-        <label for="exp-cost">Cost:</label>
+        <label>Cost:</label>
         <input type="text" id="exp-cost"></input>
-        <label for="exp-category">Category:</label>
+        <label>Category:</label>
         <input type="text" id="exp-category"></input>
         <input type="submit" value="Submit"></input>
       </form>
