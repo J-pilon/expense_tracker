2044b70bb950919e15edb2aa2c486115dc9b0f29
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 22:03:26 -0700

    added axios get request

--- client/src/Expenses.jsx
+++ client/src/Expenses.jsx
@@ -3,15 +3,15 @@ import axios from "axios";
 
 export default function Expenses() {
   useEffect(() => {
-    axios("http://localhost:8080/api/expenses")
+    axios.get("http://localhost:3001/api/expenses")
       .then(res => console.log(res.data))
       .catch(err => console.log(err));
-  })
+  }, [])
 
 
   return (
     <div>
-      
+      <p>hello</p>
     </div>
   )
 }
