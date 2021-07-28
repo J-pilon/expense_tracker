801bd0ad95a775f7967382fbd24b59b6d7369af8
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 20:22:11 -0700

    setup pooling connections

--- API/app.js
+++ API/app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+const pool = require('./db/mysql_setup')
 
 var app = express();
 
@@ -13,6 +12,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/api/expenses', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if(err) throw err;
+    console.log('connected as id ' + connection.threadId);
+
+    connection.query('SELECT * FROM expenses', (err, rows) => {
+      connection.release();
+      if(err) throw err;
+
+      console.log('The data from the expenses table \n', rows)
+    })
+  })
+})
 
 module.exports = app;
