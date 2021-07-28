474485223ac3e555dc776545cb82bf4c21b8a182
Josiah <josiah.pilon@yahoo.ca>
2021-07-28 14:45:45 -0700

    clears input fields after submit

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -6,21 +6,27 @@ export default function Form(props) {
   const [ category, setCategory ] = useState('');
   const { setNewExpense } = props;
 
+  function reset() {
+    setExpenseName('');
+    setCost('');
+    setCategory('');
+  }
+
   function submitHandler(e) {
     e.preventDefault();
-    console.log('$$', {name: expenseName, cost: cost, category: category})
     setNewExpense({name: expenseName, cost: cost, category: category})
+    reset();
   }
 
   return (
     <div>
       <form onSubmit={e => submitHandler(e)}>
         <label>Expense name:</label>
-        <input type="text" id="exp-name" onChange={e => setExpenseName(e.target.value)}></input>
+        <input type="text" id="exp-name" onChange={e => setExpenseName(e.target.value)} value={expenseName}></input>
         <label>Cost:</label>
-        <input type="text" id="exp-cost" onChange={e => setCost(e.target.value)}></input>
+        <input type="text" id="exp-cost" onChange={e => setCost(e.target.value)} value={cost}></input>
         <label>Category:</label>
-        <input type="text" id="exp-category" onChange={e => setCategory(e.target.value)}></input>
+        <input type="text" id="exp-category" onChange={e => setCategory(e.target.value)} value={category}></input>
         <input type="submit" value="Submit"></input>
       </form>
     </div>
