f55b7a9887f70306ab8d7cf66bcd7bcb6c0136c1
Josiah <josiah.pilon@yahoo.ca>
2021-07-28 07:18:28 -0700

    When submitted form, all the values from the input fields are set in setNewExpense

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,20 +1,26 @@
-export default function Form() {
+import { useState } from 'react';
+
+export default function Form(props) {
+  const [ expenseName, setExpenseName ] = useState('');
+  const [ cost, setCost ] = useState('');
+  const [ category, setCategory ] = useState('');
+  const { setNewExpense } = props;
 
   function submitHandler(e) {
     e.preventDefault();
-
-    console.log("submitted!")
+    console.log('$$', {name: expenseName, cost: cost, category: category})
+    setNewExpense({name: expenseName, cost: cost, category: category})
   }
 
   return (
     <div>
       <form onSubmit={e => submitHandler(e)}>
         <label>Expense name:</label>
-        <input type="text" id="exp-name"></input>
+        <input type="text" id="exp-name" onChange={e => setExpenseName(e.target.value)}></input>
         <label>Cost:</label>
-        <input type="text" id="exp-cost"></input>
+        <input type="text" id="exp-cost" onChange={e => setCost(e.target.value)}></input>
         <label>Category:</label>
-        <input type="text" id="exp-category"></input>
+        <input type="text" id="exp-category" onChange={e => setCategory(e.target.value)}></input>
         <input type="submit" value="Submit"></input>
       </form>
     </div>
