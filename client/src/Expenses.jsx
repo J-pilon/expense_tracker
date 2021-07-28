a35c51d23f9d51869732b25e3a19f6744b33b1cd
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 22:25:04 -0700

    after recieving expenses from api, displays them on the screen

--- client/src/Expenses.jsx
+++ client/src/Expenses.jsx
@@ -1,17 +1,20 @@
-import { useEffect } from "react";
-import axios from "axios";
+export default function Expenses(props) {
+  const { expenses } = props;
 
-export default function Expenses() {
-  useEffect(() => {
-    axios.get("http://localhost:3001/api/expenses")
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err));
-  }, [])
+  console.log("expenses", expenses);
 
-
+  const expenseList = expenses.map(exp => {
+    return (
+      <ul style={{listStyleType: "none"}}>
+        <li>{exp.title}</li>
+        <li>{exp.cost_cents}</li>
+        <li>{exp.category}</li>
+      </ul>
+    )
+  })
   return (
     <div>
-      <p>hello</p>
+      {expenseList}
     </div>
   )
 }
