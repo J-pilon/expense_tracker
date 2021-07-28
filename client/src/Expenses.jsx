7589c79c180d70b84686ee2669958c9b2a238fb2
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 17:52:00 -0700

    changed server port to 8080

--- client/src/Expenses.jsx
+++ client/src/Expenses.jsx
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 export default function Expenses() {
   useEffect(() => {
-    fetch("http://localhost:3000/api/expenses")
+    fetch("http://localhost:8080/api/expenses")
       .then(res => console.log(res))
       .catch(err => console.log(err));
   })
