eb23803ff8d198551972b4567606256613fa73b1
Josiah <josiah.pilon@yahoo.ca>
2021-07-27 20:44:40 -0700

    post route to api/expenses

--- API/db/mysql_setup.js
+++ API/db/mysql_setup.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  host     : 'localhost',
-  user     : 'renaissance',
+  connectionLimit: 2,
+  host : 'localhost',
+  user : 'renaissance',
   password : '1234',
   database : 'expense_tracker'
 });
