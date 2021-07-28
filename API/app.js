0f4b15636ca2841127db89e9c053d6c062bc5f6c
Josiah <josiah.pilon@yahoo.ca>
2021-07-28 13:38:05 -0700

    added cors middleware

--- API/app.js
+++ API/app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const pool = require('./db/mysql_setup')
+const express = require('express');
+const path = require('path');
+// const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const pool = require('./db/mysql_setup');
 
 var app = express();
 
-app.use(logger('dev'));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+app.use(logger('tiny'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/expenses', (req, res) => {
   pool.getConnection((err, connection) => {
@@ -26,13 +28,12 @@ app.get('/api/expenses', (req, res) => {
       if(err) throw err;
 
       res.send(rows);
-      console.log('The data from the expenses table \n', rows)
     })
   })
 })
 
 app.post('/api/expenses', (req, res) => {
-  console.log('params@', req.params);
+  console.log(req.body);
 
   // pool.getConnection((err, connection) => {
   //   if(err) throw err;
