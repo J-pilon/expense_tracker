3058c97c790bdf205f277d670b7f0ada27259562
Josiah <josiah.pilon@yahoo.ca>
2021-07-28 19:51:51 -0700

    added placeholder to cost input

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -24,7 +24,7 @@ export default function Form(props) {
         <label>Expense name:</label>
         <input type="text" id="exp-name" onChange={e => setExpenseName(e.target.value)} value={expenseName}></input>
         <label>Cost:</label>
-        <input type="text" id="exp-cost" onChange={e => setCost(e.target.value)} value={cost}></input>
+        <input type="text" id="exp-cost" placeholder="$" onChange={e => setCost(e.target.value)} value={cost}></input>
         <label>Category:</label>
         <input type="text" id="exp-category" onChange={e => setCategory(e.target.value)} value={category}></input>
         <input type="submit" value="Submit"></input>
