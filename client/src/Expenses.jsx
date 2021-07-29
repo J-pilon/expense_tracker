5991f82dd7b06d36565fe1d3604ae2d83b1d2f3e
Josiah <josiah.pilon@yahoo.ca>
2021-07-28 19:40:04 -0700

    added total of costs

--- client/src/Expenses.jsx
+++ client/src/Expenses.jsx
@@ -3,17 +3,25 @@ import "./styles/expenses.css";
 export default function Expenses(props) {
   const { expenses, setDeleteExpense } = props;
 
-  // console.log("expenses", expenses);
-
   function clickHandler(id) {
     setDeleteExpense(id);
   }
 
+  function totalOfCosts() {
+    let totalCost = 0;
+    for (const expense of expenses) {
+      totalCost += expense.cost_cents;
+    }
+    return totalCost / 100;
+  }
+
+  
   const expenseList = expenses.map((exp, index) => {
+    const price = exp.cost_cents / 100;
     return (
       <tr key={index} id={index}>
         <td>{exp.title}</td>
-        <td>{exp.cost_cents}</td>
+        <td>${price}</td>
         <td>{exp.category}</td>
         <td><button className="delete-button" type="button" onClick={() => clickHandler(exp.id)}>Delete</button></td>
       </tr>
@@ -32,6 +40,10 @@ export default function Expenses(props) {
 
         <tbody>
           {expenseList}
+          <tr>
+            <td>Total</td>
+            <td>${totalOfCosts()}</td>
+          </tr>
         </tbody>
       </table>
     </div>
